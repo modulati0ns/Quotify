2b9ed68f999e040855cf7ba18a3195de621c7ed5
modulati0ns <javimtmz96@gmail.com>
2020-07-07 12:06:25 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const button = document.querySelector('.button')
+const text = document.querySelector('.quote');
+const autor = document.querySelector('.autor');
+button.addEventListener("click", getQuotes);
+
+
+async function getQuotes() {
+    // const response = await fetch("https://type.fit/api/quotes");
+    // const quotes = await response.json();
+    // const quote = await quotes.data;
+    // console.log(quote);
+
+    fetch("https://type.fit/api/quotes")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let number = Math.floor(Math.random() * data.length);
+            let quote = data[number].text;
+            let author = data[number].author;
+            if (author == null) {
+
+
+                console.log("Sin autor, buscando otra cita");
+                getQuotes();
+            } else {
+                text.innerHTML = '"' + quote + '"';
+                autor.innerHTML = '- ' + author;
+                console.log(author);
+            }
+
+
+        });
+
+}
